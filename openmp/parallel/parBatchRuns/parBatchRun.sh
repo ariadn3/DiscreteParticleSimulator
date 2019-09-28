33e45f67b3e03c2c55a75b63954de92874743a97
ariadn3 <threadofethariadne@gmail.com>
2019-09-29 02:58:51 +0800

    Add test cases for parallel

---
+++ openmp/parallel/parBatchRuns/parBatchRun.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+python3 generateTestcases.py
+
+TOTAL_RUNS=5
+
+rm *.seq -f
+
+allThreads=(1 2 4 6 8 10 12 16 20 24 28 32 40 64)
+
+for numThreads in ${allThreads[@]}
+do
+	echo "Running on $numThreads threads..."
+	for inputFile in *.in
+	do
+		truncFilepath=${inputFile%.in}
+		echo -e "\tRunning parallel $truncFilepath..."
+		runTrial=1
+		while [ $runTrial -le $TOTAL_RUNS ]
+		do
+			echo -e "\t\tTrial $runTrial..."
+			perf stat -o "$truncFilepath-$numThreads-trial$runTrial.par" ../a.out $numThreads < $inputFile > /dev/null
+			((runTrial++))
+		done
+	done
+done
