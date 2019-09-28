8a9e634b837a09199d79365b489dc8781701fbb0
ariadn3 <threadofethariadne@gmail.com>
2019-09-28 19:57:09 +0800

    Animation generation QoL; Added test cases

--- visualisations/generateAnimation.py
+++ visualisations/generateAnimation.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from math import pi
 from time import time
+import sys
 
-filepath = input('Enter the input/output file you would like to visualize (xxx.in & xxx.out): ')
+if len(sys.argv) > 1:
+	filepath = sys.argv[1]
+else:
+	filepath = input('Enter the input/output file you would like to visualize (xxx.in & xxx.out): ')
 # filepath = 'testAnim'
+
+_SLOWDOWN_FACTOR = 20
 
 startTime = time()
 
@@ -23,18 +29,24 @@ with open(filepath + '.in', 'r') as particleIn:
 	r = float(particleIn.readline().strip())
 	steps = int(particleIn.readline().strip())
 
+steps *= _SLOWDOWN_FACTOR
+maxSpeed = L/4/_SLOWDOWN_FACTOR
+
 with open(filepath + '.out', 'r') as particleOut:
 	for line in particleOut.readlines():
 		data = line.strip().split()
-		step, index, x, y, _, _ = data
+		step, index, x, y, v_x, v_y = data
 		step = int(step)
 		index = int(index)
 		x = float(x)
 		y = float(y)
+		v_x = float(v_x)
+		v_y = float(v_y)
+		v = (v_x**2 + v_y**2)**0.5
 
 		if step not in dataDict:
 			dataDict[step] = {}
-		dataDict[step][index] = (x, y)
+		dataDict[step][index] = (x, y, v)
 
 for i in range(steps+1):
 	if i not in dataDict:
@@ -43,6 +55,7 @@ for i in range(steps+1):
 		print('WARNING: Unequal number of particles (detected at timestep {})'.format(i))
 
 fig, ax = plt.subplots(figsize=(8, 8))
+time_text = ax.text(0.8, 1.05,'',horizontalalignment='left',verticalalignment='top', transform=ax.transAxes, size = 14)
 
 def init():
 	ax.set_xlim(0, L)
@@ -52,11 +65,14 @@ def init():
 circlePatch = []
 def plotPoints(step):
 	circles = []
+	time_text.set_text('Frame: {}'.format(step))
+	circles.append(time_text)
 	for patch in circlePatch:
 		patch.remove()
 	circlePatch.clear()
 	for v in dataDict[step].values():
-		newPatch = plt.Circle(v, r)
+		scale = max(min(v[2] / maxSpeed, 1), 0)
+		newPatch = plt.Circle(v[:2], r, color = (scale, 0, 1-scale))
 		circlePatch.append(newPatch)
 		circles.append(ax.add_patch(newPatch))
 	# scatter.set_offsets(list(dataDict[step].values()))
