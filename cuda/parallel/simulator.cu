20ec2735c19bfff07f5bb2cc275463018f681161
Aulud <44989315+Aulud@users.noreply.github.com>
2019-10-20 17:35:19 +0800

    Increase floating-point epsilon for CUDA

--- cuda/parallel/simulator.cu
+++ cuda/parallel/simulator.cu
@@ -5,7 +5,7 @@
 
 #define SLOW_FACTOR 1
 #define NO_COLLISION 2
-#define EDGE_TOLERANCE 1e-14
+#define EDGE_TOLERANCE 1e-8
 
 __host__ void simulate();
 __host__ void printAll(bool, int);
@@ -248,8 +248,10 @@ __global__ void checkWallCollision() {
     
     if (index >= n)
         return;
-
+    
     particle_t p = ps[index];
+    
+    // printf("Thread %d checking particle %d\n", index, p.id);
 
     // Collision times with vertical and horizontal walls
     double x_time = NO_COLLISION;
@@ -306,8 +308,6 @@ __global__ void checkCollision() {
     int pIndex = blockIdx.x;
     int qIndex = blockDim.x * blockIdx.y + threadIdx.x;
 
-    // printf("Checking array computation (%d, %d)\n", pIndex, qIndex);
-
     particle_t p, q;
     
     // Ignore excess threads beyond the row of computation
@@ -319,14 +319,16 @@ __global__ void checkCollision() {
         q = ps[qIndex];
     } else if (gridDim.x % 2 == 0 || pIndex != gridDim.x - 1) {
         // Compute reflected lower half of triangle folded to form a row
-        p = ps[n - 1 - qIndex];
-        q = ps[n - 1 - pIndex];
+        p = ps[n - 2 - pIndex];
+        q = ps[n - 1 - qIndex];
     } else {
         // Catch case where n is even -> odd number of rows of computation when folded
         // Ignore excess threads beyond the middle column
         return;
     }
 
+    // printf("Checking array computation (%d, %d)\n", p.id, q.id);
+    
     // Difference in X and Y positions and velocities of particles P, Q
     double dX = q.x - p.x;
     double dY = q.y - p.y;
@@ -378,8 +380,8 @@ __global__ void settleCollision() {
     collision_t curCollision = cs[collIndex];
 
     // Particles A and B (null if wall collision) in this collision
-    particle_t* __restrict__ A = curCollision.p;
-    particle_t* __restrict__ B = curCollision.q;
+    particle_t* A = curCollision.p;
+    particle_t* B = curCollision.q;
     double time = curCollision.time;
 
     // Advance A by the fractional time step dT until collision occurs
@@ -388,13 +390,17 @@ __global__ void settleCollision() {
 
     // If the collision is against the wall, toggle directions
     if (B == NULL) {
+        if (false && (A->id == 976 || A->id == 975)) {
+            printf("Particle %d: %.20lf %.20lf\n", A->id,
+            A->x - r - EDGE_TOLERANCE, A->y - r - EDGE_TOLERANCE);
+        }
         // Add to wall collision counter of A
         A->w_collisions += 1;
         // printf("Step %.14lf: particle %d collided with wall\n", time, A->id);
         if (A->x <= r + EDGE_TOLERANCE || A->x >= l - r - EDGE_TOLERANCE)
-            A->v_x *= -1;
+            A->v_x = A->v_x * -1;
         if (A->y <= r + EDGE_TOLERANCE || A->y >= l - r - EDGE_TOLERANCE)
-            A->v_y *= -1;
+            A->v_y = A->v_y * -1;
     }
     // If collision is against another particle
     else {
@@ -464,7 +470,9 @@ __global__ void settleCollision() {
 
     // If particle A will collide against the wall, check when it will collide
     // with the nearest wall and take that time
-    double time_ax = 1 - time, time_ay = 1 - time;
+    double time_ax = 1 - time;
+    double time_ay = 1 - time;
+    
     if (A->v_x != 0) {
         if (A->x + time_ax * A->v_x < r) time_ax = -(A->x - r) / A->v_x;
         else if (A->x + time_ax * A->v_x > l - r) time_ax = (l - r - A->x) / A->v_x;
@@ -484,11 +492,14 @@ __global__ void settleCollision() {
 
 // Updates particles not involved in any collision
 __global__ void updateParticles() {
-    int index = blockIdx.x * gridDim.x + threadIdx.x;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
     if (index >= n)
         return;
 
     particle_t* curParticle = &ps[index];
+    
+    // printf("Thread %d updating particle %d\n", index, curParticle->id);
     
     if (!states[index]) {
         // Advance particle by its velocity
